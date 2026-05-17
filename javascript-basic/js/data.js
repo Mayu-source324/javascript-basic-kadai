416d49d7cd189de8cdd1ce2219db8fa02935f43e
Mayu-source324 <orangelemon1216@gmail.com>
2026-05-17 21:54:09 +0900

    課題をアップロード（四則演算）
    課題をアップロード（四則演算）

---
+++ javascript-basic/js/data.js
@@ -0,0 +1,16 @@
+//四則演算
+console.log(45 + 18);
+console.log(30 -12);
+console.log(15 * 6);
+console.log(30 / 3);
+console.log(45 % 8);
+
+//文字列の連結
+console.log('45' + '18');
+console.log('今日の天気は' + '晴れ');
+console.log('今日お天気は' + '曇り');
+console.log('ジパング一の伊達男');
+console.log('菊五郎は' + '3丁目');
+console.log('アニメの世界は111');
+console.log(10-5 + 'は数値型データです');
+
