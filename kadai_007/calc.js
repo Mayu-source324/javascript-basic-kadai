e487a55ea3c7305d808cbf0d35860a105e0b85bd
Mayu-source324 <orangelemon1216@gmail.com>
2026-06-04 00:46:29 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -11,9 +11,9 @@ if(num % 5 === 0){
     console.log('5の倍数です');
 }
 // 変数numが3と5の倍数の場合： “3と5の倍数です”
-if (num % 3 || num % 5 === 0){
+if (num % 3 === 0) && (num % 5 === 0){
     console.log('3と5の倍数です');
 }
 else {
-    console.log('num');
+    console.log(num);
 }
