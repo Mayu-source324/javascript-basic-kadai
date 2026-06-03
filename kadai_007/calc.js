41aeef8610f4657376c0c8c172ffabf26b9ac335
Mayu-source324 <orangelemon1216@gmail.com>
2026-06-04 00:27:16 +0900

    課題をアップロード
    課題をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,19 @@
+// 任意の数値を代入しておく。
+let num = 5;
+
+console.log(num);
+// 変数numが3の倍数の場合： “3の倍数です”
+if (num % 3=== 0){
+    console.log('3の倍数です');
+}
+// 変数numが5の倍数の場合： “5の倍数です”
+if(num % 5 === 0){
+    console.log('5の倍数です');
+}
+// 変数numが3と5の倍数の場合： “3と5の倍数です”
+if (num % 3 || num % 5 === 0){
+    console.log('3と5の倍数です');
+}
+else {
+    console.log('num');
+}
